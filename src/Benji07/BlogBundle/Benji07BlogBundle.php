25ff82da9feeb45984527b35f5fb87a5a7e49b99
Benjamin Lévêque <benjamin@leveque.me>
2012-03-11 11:48:12 +0100

    Ajout BlogBundle

---
+++ src/Benji07/BlogBundle/Benji07BlogBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Benji07\BlogBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Benji07 BlogBundle
+ */
+class Benji07BlogBundle extends Bundle
+{
+}
---
+++ src/Benji07/BlogBundle/Controller/DefaultController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Benji07\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * Benji07BlogBundle DefaultController
+ */
+class DefaultController extends Controller
+{
+}
