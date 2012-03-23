27ac97f2306f98dd69143204e99142a6cdccdc43
Benjamin Lévêque <benjamin@leveque.me>
2012-03-23 13:09:56 +0100

    Fix login

--- src/Benji07/MainBundle/Controller/DefaultController.php
+++ src/Benji07/MainBundle/Controller/DefaultController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use Symfony\Component\Security\Core\SecurityContext;
+
 /**
  * Main Bundle Default Controller
  */
@@ -44,5 +46,53 @@ class DefaultController extends Controller
         $projects = $r->findBy(array('active' => true), array('createdAt' => 'DESC'));
 
         return array('projects' => $projects);
+    }
+
+    /**
+     * Login action
+     *
+     * @Route("/admin/login", name="login")
+     * @Template()
+     *
+     * @return array
+     */
+    public function loginAction()
+    {
+        $request = $this->getRequest();
+        $session = $request->getSession();
+
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+        return array(
+            'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+            'error'         => $error
+        );
+    }
+
+    /**
+     * Login check action
+     *
+     * @Route("/admin/login_check", name="login_check")
+     *
+     * @return void
+     */
+    public function loginCheckAction()
+    {
+    }
+
+    /**
+     * Login action
+     *
+     * @Route("/admin/logout", name="logout")
+     *
+     * @return void
+     */
+    public function logout()
+    {
     }
 }
